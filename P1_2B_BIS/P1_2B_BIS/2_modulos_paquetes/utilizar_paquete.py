08d6350ac990f89a3dcf2381f2bf710bdfa108e5
BustilloDiego3B <diego_3141240180@utd.edu.mx>
2025-07-23 08:33:45 -0600

    Delete P1_2B_BIS directory

--- P1_2B_BIS/P1_2B_BIS/2_modulos_paquetes/utilizar_paquete.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Diego Bustillo Aguirre"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.SolicitarDatos2()
-print(f"\n\t.::Agenda Telefonica::.\n\tNombre: {nom} \n\t\tTelefono:{tel}")
-modulos.espereTecla()
